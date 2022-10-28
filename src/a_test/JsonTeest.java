9ff775774ff971141d2a2275b79a553efd43cf9d
Hansel <rogitun@gmail.com>
2022-10-28 23:21:29 +0900

    test

---
+++ src/a_test/JsonTeest.java
@@ -0,0 +1,15 @@
+package a_test;
+
+import java.util.ArrayList;
+
+public class JsonTeest {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        ArrayList<Object> objects = new ArrayList<>();
+        System.out.println(objects.size());
+    }
+}
