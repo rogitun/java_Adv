4cb37c6fe930983c7a1dae982a72bb7d2787e7e3
Hansel <rogitun@gmail.com>
2023-03-26 22:28:29 +0900

    Java_8 - testcode

--- src/test.java
+++ src/test.java
@@ -1,15 +1,35 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import java.util.stream.Stream;
 
 public class test {
-    public static void main(String[] args) {
-        int i =  50;
-         List<Integer> list = Arrays.asList(1,2,3,4,5);
+	public static void main(String[] args) throws IOException {
+		int[] numbers = new int[5_000_001];
+		for (int i = 0; i < 5_000_001; i++) {
+			numbers[i] = i;
+		}
 
-        List<String> collect = list.stream().map(String::valueOf).collect(Collectors.toList());
+		// for (int j = 0; j < 10; j++) {
+		// 	long before = System.currentTimeMillis();
+		// 	int max = Integer.MIN_VALUE;
+		// 	for (int i = 0; i < 500_001; i++) {
+		// 		if (numbers[i] > max)
+		// 			max = numbers[i];
+		// 	}
+		// 	long after = System.currentTimeMillis();
+		// 	System.out.println(after - before + "ms");
+		// }
 
-
-    }
+		for (int j = 0; j < 10; j++) {
+			long before = System.currentTimeMillis();
+			Arrays.stream(numbers)
+				.reduce(Integer.MIN_VALUE, Math::max);
+			long after = System.currentTimeMillis();
+			System.out.println(after - before + "ms");
+		}
+	}
 }
