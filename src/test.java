dc1008f747bd17ec7585df05b2a208398a978d2b
hansel <rogitun@gmail.com>
2023-03-26 22:45:57 +0900

    asdsadsad

--- src/test.java
+++ src/test.java
@@ -7,29 +7,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class test {
-	public static void main(String[] args) throws IOException {
-		int[] numbers = new int[5_000_001];
-		for (int i = 0; i < 5_000_001; i++) {
-			numbers[i] = i;
-		}
+    public static void main(String[] args) throws IOException {
+        int[] numbers = new int[5_000_001];
+        for (int i = 0; i < 5_000_001; i++) {
+            numbers[i] = i;
+        }
 
-		// for (int j = 0; j < 10; j++) {
-		// 	long before = System.currentTimeMillis();
-		// 	int max = Integer.MIN_VALUE;
-		// 	for (int i = 0; i < 500_001; i++) {
-		// 		if (numbers[i] > max)
-		// 			max = numbers[i];
-		// 	}
-		// 	long after = System.currentTimeMillis();
-		// 	System.out.println(after - before + "ms");
-		// }
+        for (int j = 0; j < 10; j++) {
+            long before = System.nanoTime();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < 5_000_001; i++) {
+                if (numbers[i] > max)
+                    max = numbers[i];
+            }
+            long after = System.nanoTime();
+            System.out.println((after - before)/100_000 + "ms");
+        }
 
-		for (int j = 0; j < 10; j++) {
-			long before = System.currentTimeMillis();
-			Arrays.stream(numbers)
-				.reduce(Integer.MIN_VALUE, Math::max);
-			long after = System.currentTimeMillis();
-			System.out.println(after - before + "ms");
-		}
-	}
+//        for (int j = 0; j < 10; j++) {
+//            long before = System.nanoTime();
+//            Arrays.stream(numbers)
+//                    .reduce(Integer.MIN_VALUE, Math::max);
+//            long after = System.nanoTime();
+//            System.out.println((after - before) / 100_000 + "ms");
+//        }
+    }
 }
